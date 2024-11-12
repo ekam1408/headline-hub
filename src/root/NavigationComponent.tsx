09270a166151b0549e6fbe3f47aa16abef0e1320
Ekam <esingh0406@gmail.com>
2024-11-12 23:01:26 +0530

    added empty screens inside stack nav

---
+++ src/root/NavigationComponent.tsx
@@ -0,0 +1,28 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import SplashScreen from '../screens/SplashScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+enum Routes {
+  Splash = 'Splash',
+  Home = 'Home',
+}
+
+const NavigationComponent = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name={Routes.Splash}
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name={Routes.Home} component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default NavigationComponent;
